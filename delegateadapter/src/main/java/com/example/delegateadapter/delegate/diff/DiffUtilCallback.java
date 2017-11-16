07eb0ecd2ff5464f766a05b57c5ab3f4a1146c5d
dumchev <dumchev@yandex-team.ru>
2017-11-16 11:06:48 +0300

    add DiffUtil implementation

---
+++ delegateadapter/src/main/java/com/example/delegateadapter/delegate/diff/DiffUtilCallback.java
@@ -0,0 +1,42 @@
+package com.example.delegateadapter.delegate.diff;
+
+import android.support.annotation.NonNull;
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+/**
+ * @author dumchev on 16.11.17.
+ */
+final class DiffUtilCallback extends DiffUtil.Callback {
+
+    private final List<? extends IComparableItem> oldList;
+    private final List<? extends IComparableItem> newList;
+
+    DiffUtilCallback(@NonNull List<? extends IComparableItem> oldList,
+                     @NonNull List<? extends IComparableItem> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).id().equals(newList.get(newItemPosition).id());
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).content().equals(newList.get(newItemPosition).content());
+    }
+}
+
---
+++ delegateadapter/src/main/java/com/example/delegateadapter/delegate/diff/IComparableItem.java
@@ -0,0 +1,21 @@
+package com.example.delegateadapter.delegate.diff;
+
+/**
+ * @author dumchev on 16.11.17.
+ */
+public interface IComparableItem {
+
+    /**
+     * {@link DiffUtilCallback} uses this method to distinguish between items
+     *
+     * @return item's unique id that won't be changed
+     */
+    Object id();
+
+    /**
+     * {@link DiffUtilCallback} uses this method to know, that item has changed
+     *
+     * @return data that represents visual part of the item
+     */
+    Object content();
+}
